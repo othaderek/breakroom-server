7a79827ff9885cf855a24e398857f7bd6fddf9f2
othaderek <otha.hernandez@gmail.com>
2021-02-26 18:41:05 -0500

    Adding some changes.

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const socketIO = require("socket.io");
 const app = express();
-const server = require('http').createServer(app);
+const http = require('http');
+const server = http.createServer(app);
+const { v4: uuidV4 } = require('uuid');
+const expressVue = require("express-vue");
 
 const port = 5000;
+
+console.log(server.getMaxListeners());
 
 const io = socketIO(server, {
     cors: {
@@ -15,19 +20,28 @@ const io = socketIO(server, {
 });
 
 app.get('/', (req, res) => {
-    res.send('Connecting to chat room...')
+    // res.redirect(`/${uuidV4()}`);
+    // Checks connected clients to server.
 });
 
-app.get('/:room', (req, res) => {
-    res.render('room', {roomId: req.params.room})
-})
+// app.get('/:room', (req, res) => {
+//     const roomId = req.params.room
+//     res.send('room', roomId);
+// })
 
 io.on("connection", socket => {
-    console.log("socket connected")
+    console.log("socket connecting..")
+
     socket.on('mounted', data => { 
-        console.log('data: ', data)
+        console.log('data: ', data);
+        console.log('connected clients: ', io.sockets.adapter.rooms);
         io.emit('test');
     });
+
+    socket.on('join-room', (roomId, userId) => {
+        console.log(roomId, userId);
+    })
+
 });
 
 server.listen(port, () => {
